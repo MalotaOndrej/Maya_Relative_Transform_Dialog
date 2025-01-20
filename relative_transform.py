5c4b1ada6e18499129d0beb84cbcba07570b1508
Ondrej Malota <127194072+MalotaOndrej@users.noreply.github.com>
2025-01-20 08:43:49 +0100

    widget_to_dialog_type

--- relative_transform.py
+++ relative_transform.py
@@ -9,16 +9,14 @@ def get_maya_window():
     if ptr is not None:
         return shiboken2.wrapInstance(int(ptr), qg.QWidget)
 
-class SetPosition(qg.QWidget):
+class SetPosition(qg.QDialog):
     def __init__(self, parent=None):
-        super(SetPosition, self).__init__(parent)
+        super(SetPosition, self).__init__(parent, qc.Qt.FramelessWindowHint)
         
         self.setWindowTitle("Set Position")
-        self.setWindowFlags(qc.Qt.WindowStaysOnTopHint | qc.Qt.FramelessWindowHint)
-        self.setWindowOpacity(0.75)
+        self.setWindowOpacity(1)
         self.setFixedWidth(150)
         self.setFixedHeight(40)
-        self.keyPressEvent = self.KeyPress
         
         layout = qg.QGridLayout(self)
         
@@ -36,7 +34,7 @@ class SetPosition(qg.QWidget):
         self.timer.timeout.connect(self.check_inputs)
         self.timer.start(1)  # Check every second
 
-    def KeyPress(self, event):
+    def keyPressEvent(self, event):
         if event.key() in (qc.Qt.Key_Return, qc.Qt.Key_Enter):
             cmds.move(self.entry_x.text() if self.entry_x.text() else 0,
                       self.entry_y.text() if self.entry_y.text() else 0,
@@ -46,31 +44,23 @@ class SetPosition(qg.QWidget):
         elif event.key() == qc.Qt.Key_Escape:
             self.hide()
 
-    def eventFilter(self, obj, event):
-        if event.type() == qc.QEvent.MouseButtonPress:
-            global_pos = event.globalPos()
-            if not self.geometry().contains(global_pos):
-                self.hide()
-                return True
-        return super(SetPosition, self).eventFilter(obj, event)
+    def mousePressEvent(self, event):
+        if not self.rect().contains(event.pos()):
+            self.hide()
 
     def check_inputs(self):
-        # Check if none of the inputs have focus
         if not self.entry_x.hasFocus() and not self.entry_y.hasFocus() and not self.entry_z.hasFocus():
             self.hide()
 
-# Create an instance of the widget
-widget = SetPosition(get_maya_window())
+# Create an instance of the dialog
+dialog = SetPosition(get_maya_window())
 
-# Install the event filter on the widget itself
-widget.installEventFilter(widget)
+# Show the dialog
+dialog.show()
 
-# Show the widget
-widget.show()
+# Set focus to the first input field after showing the dialog
+dialog.entry_x.setFocus()
 
-# Set focus to the first input field after showing the widget
-widget.entry_x.setFocus()
-
-# Center the widget in the Maya window
+# Center the dialog in the Maya window
 maya_window = get_maya_window()
-widget.move(maya_window.frameGeometry().center() - widget.rect().center())
+dialog.move(maya_window.frameGeometry().center() - dialog.rect().center())
